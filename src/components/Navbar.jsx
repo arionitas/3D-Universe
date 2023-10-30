06e465cf4cf00ac8dbbd04a8660f3326a4a49071
arionitas <mgmt@arxivn.com>
2023-10-30 14:25:40 +0000

    added navbar navigation hover elements and click elements. upon hover, the mouse interacts with the navbar in a way that showcasees you are selecting that part of the menu by highlighting the option in white

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,13 +10,13 @@ const Navbar = () => {
   return (
     <nav
       className={`${styles.paddingX} w-full flex
-      items-center py-5 fixed top-0 z-20`}
+      items-center py-5 top-0 z-20`}
     >
       <div classNaME="w-full flex justify-between
       items-center max-w-7xl mmx-auto">
         <Link
           to="/"
-          className="flex items-center gap-2"
+          className="flex items-center gap-3"
           onClick={() => {
             setActive("");
             window.scrollTo(0,0);
@@ -24,10 +24,30 @@ const Navbar = () => {
         >
           <img src={logo} alt="logo" className="w-9
           h-9 object-contain" />
+          <p className="text-white text-[18px]
+          font-bold cursor-pointer">Arion Mehmeti</p>
         </Link>
+        <ul className="list-none hidden sm:flex
+        flex-row gap-10">
+          {navLinks.map((link) => (
+            <li
+              key={link.id}
+              className={`${
+                active === link.title
+                ? "text-white"
+                : "text-secondary"
+              }  hover:text-white text-[18px]
+                 font-medium cursor-pointer`}
+                 onClick={() => setActive(link.title)}
+            >
+              <a href={`#${link.id}`}>{link.title}</a>
+            </li>
+          ))}
+        </ul>
       </div>
     </nav>
   )
 }
+
 
 export default Navbar
