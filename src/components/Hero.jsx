ecd1355e7be668d0515f09a485b348f3e032976c
arionitas <mgmt@arxivn.com>
2023-10-31 15:44:49 +0000

    saving work and imported stylistic elements from folder structure ready to implement

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,13 @@
-import React from 'react'
+import { motion } from 'framer-motion';
+
+import { styles } from '../styles';
+import { ComputerCanvas} from './canvas';
+
+
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <<div>Hero</div>>
   )
 }
 
