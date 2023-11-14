4fa05e5fac26ab3ce351a47d5d6bd7b95c54b9be
arionitas <mgmt@arxivn.com>
2023-11-14 17:13:09 +0000

    added scroller animations to interact with the tilted card animations

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,6 +5,7 @@ import { motion } from 'framer-motion';
 import { styles } from '../styles';
 import { services } from '../constants';
 import { fadeIn, textVariant } from '../utils/motion';
+import { SectionWrapper } from '../hoc';
 
 const ServiceCard = ({ index, title, icon}) => {
   return (
@@ -69,4 +70,4 @@ const About = () => {
   )
 }
 
-export default About
+export default SectionWrapper(About, "about")
